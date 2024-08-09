ab21217d69a1a50c7afd38af1e0b1ed4c6523042
Erick Oliveira <erick.oliveira.eu@gmail.com>
2024-08-09 19:56:18 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,18 +4,17 @@ function tocaSom (idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-
     const idAudio = `#som_${instrumento}`; 
 
     tecla.onclick = function() {
         tocaSom(idAudio);
     }
 
-    contador = contador + 1;
+    tecla.onkeydown = function () {
+        tecla.classList.add('ativa');
+    }
 }
