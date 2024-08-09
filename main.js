6333f1748de33ea8675304840eaf073cf1fb8353
Erick Oliveira <erick.oliveira.eu@gmail.com>
2024-08-08 22:10:25 -0300

    Update main.js

--- main.js
+++ main.js
@@ -7,11 +7,15 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 let contador = 0;
 
 while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = function() {
-        tocaSom('#som_tecla_psh');
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`; 
+
+    tecla.onclick = function() {
+        tocaSom(idAudio);
     }
 
     contador = contador + 1;
-
-    console.log(contador);
 }
